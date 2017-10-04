1768b03c04d254e3056e0dc4ec1f334b0b2e4518
Nauras Jabari <nauras@moogmedia.ca>
2017-10-04 11:56:52 -0700

    ProjectTiles display correctly

--- src/components/ProjectTile.vue
+++ src/components/ProjectTile.vue
@@ -1,5 +1,10 @@
 <template>
   <router-link class="project-tile"  :to="{ name: 'PageProject', params: { slug: project.slug }, query: { id: project.id } }">
+    <div class="project-tile-overlay">
+      <div class="project-tile-title" v-html="project.title.rendered"></div>
+      <div class="project-tile-status" v-html="project.current_status"></div>
+    </div>
+    <div class="project-tile-thumbnail" :style="{ 'background-image': 'url(' + project.thumbnail.guid + ')' }"></div>
   </router-link>
 </template>
 
