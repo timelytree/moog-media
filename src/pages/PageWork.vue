458916070ded81a1d26beee237aff74f00b3d06d
Nauras Jabari <nauras@moogmedia.ca>
2017-10-04 19:54:38 -0700

    Built Work page

--- src/pages/PageWork.vue
+++ src/pages/PageWork.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="page-overlay" id="workPAGE">
-    <div class="work-nav container">
-      <div class="work-nav-title">Our Work</div>
-      <router-link to="/"><img src="../assets/images/close_rounded_icon_white.svg" /></router-link>
+  <transition name="slide">
+    <div class="page-overlay" id="workPAGE">
+      <div class="work-nav container">
+        <div class="work-nav-title">Our Work</div>
+        <router-link to="/"><img src="../assets/images/close_rounded_icon_white.svg" /></router-link>
+      </div>
+      <div class="page-modal container">
+        <ProjectTile v-for="project in projects" v-bind:project="project" :key="project.id" />
+      </div>
     </div>
-    <div class="page-modal container">
-      <ProjectTile v-for="project in projects" v-bind:project="project" :key="project.id" />
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -31,26 +33,46 @@ export default {
 
   mounted () {
     this.fetchAllProjects(response => {
-      console.log(response)
       this.projects = response
-      // this.posts = response
-      // this.loading = false
-      // var posts = this.cE('post-tile')
-      // var num1 = 0
-      // var num2 = 150
-      // var interval = setInterval(() => {
-      //   if (num1 < 5) {
-      //     this.addC(posts[num1], 'active')
-      //     num1 += 1
-      //     num2 += 25
-      //   } else {
-      //     clearInterval(interval)
-      //   }
-      // }, num2)
+      var posts = this.cE('project-tile')
+      var num1 = 0
+      var num2 = 50
+      var interval = setInterval(() => {
+        if (num1 < posts.length) {
+          this.addC(posts[num1], 'active')
+          num1 += 1
+          num2 += 25
+        } else {
+          clearInterval(interval)
+        }
+      }, num2)
     })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.slide-enter-active, .slide-leave-active {
+  transition: 400ms;
+  background-color: rgba(0, 0, 0, 0.75);
+  .work-nav, .page-modal {
+    transition: 400ms;
+    transform: translateY(0);
+  }
+  .work-nav {
+    opacity: 1;
+  }
+}
+.slide-enter, .slide-leave-to {
+  background-color: rgba(0, 0, 0, 0);
+  .work-nav, .page-modal {
+    transform: translateY(100%);
+  }
+  .work-nav {
+    opacity: 0;
+  }
+}
+</style>
 
 
 
