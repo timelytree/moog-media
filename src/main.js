9a4ea5dded38088e2a7c0d570d017285313e649b
Nauras Jabari <nauras@moogmedia.ca>
2017-10-06 09:51:09 -0700

    Implemented Google Analytics

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAnalytics from 'vue-analytics'
 import router from './router'
 import 'bootstrap/scss/bootstrap.scss'
 import './stylesheets/main.scss'
 
 Vue.config.productionTip = false
+if (window.location.hostname !== 'localhost') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-92177503-1',
+    router
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
