5fe9c13864dbdb95d42498dce8e42b593994d5e1
Nauras Jabari <nauras@moogmedia.ca>
2019-12-21 17:24:31 -0500

    Feat: projectList now fetched and Project component scaffold is displayed

---
+++ components/Project.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="project">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    metadata () {
+      return this.project.metadata
+    },
+    name () {
+      return this.project.post_title
+    },
+    tileImage () {
+      return this.metadata.tile_image
+    },
+    tileImagePlacement () {
+      return this.metadata.tile_image_placement
+    },
+    status () {
+      return this.metadata.status
+    },
+    press () {
+      const press = this.metadata.press
+      if (press) { return press }
+      return false
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@
 
     <SiteNav />
 
+    <Project
+      v-for="project in projectList"
+      :key="project.ID"
+      :project="project" />
+
     <SiteFooter />
 
   </div>
@@ -14,39 +19,31 @@ import { mapGetters } from 'vuex'
 import Api from '@/api'
 
 import SiteNav from '@/components/SiteNav'
+import Project from '@/components/Project'
 import SiteFooter from '@/components/SiteFooter'
 
 export default {
   components: {
     SiteNav,
+    Project,
     SiteFooter
-  },
-
-  data () {
-    return {
-      post: false
-    }
   },
 
   computed: {
     ...mapGetters({
       navigationPanel: 'navigation/navigationPanel',
-      siteTheme: 'global/siteTheme'
+      siteTheme: 'global/siteTheme',
+      projectList: 'posts/projectList'
     })
   },
-
-  // async asyncData ({ params, error, store }) {
-  //   const post = await Api.getSingleCptPost(params.pathMatch)
-  //   const caseStudyList = await Api.getAllCptPosts('case-study')
-  //   store.dispatch('posts/setCaseStudyList', caseStudyList.posts)
-  //   return { post }
-  // },
 
   async fetch ({ store, params }) {
     const navigationList = await Api.getNavigationList()
     const siteOptions = await Api.getSiteOptions()
+    const projectList = await Api.getAllCptPosts('project')
     store.dispatch('navigation/setNavigationList', navigationList)
     store.dispatch('global/setSiteOptions', siteOptions)
+    store.dispatch('posts/setProjectList', projectList.posts)
   }
 }
 </script>
